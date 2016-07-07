2900fc24b8bdb4c572884853328f8f401bff1dc5
Kirill93 <kirill.belash93@yandex.ru>
2016-07-07 22:37:34 +0600

    Init

---
+++ post.php
@@ -0,0 +1,11 @@
+﻿<?php
+$name = trim($_POST['name']);
+$tel = trim($_POST['tel']);
+$message = trim($_POST['message']);
+  $send = mail("partisan_sait@mail.ru", "Сообщение с сайта http://partisan54.ru/", "Имя: $name\nТелефон: $tel\nСообщение: $message\n");
+  if ($send) {
+    echo "Спасибо за Ваше обращение!";
+  } else {
+    echo "Ошибка!";
+  }
+?>
